d58524864abd5e49a5ef57a1a9e7c71066534673
Y27ali <66620ali@gmail.com>
2023-04-06 10:57:10 -0400

    Added Rectangle and Triangle Unit Test Folders

---
+++ RectangleUnitTests/RectangleUnitTests.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestGetRectanglePoints)
+		{
+			//int getRectanglePoints[3];
+
+			////tesst with valid
+
+			//int input1[3] = { 3,4,5 };
+			//int* result = getRectanglePoints(input1);
+			//Assert::AreEqual(input1[0], result[0]);
+			//Assert::AreEqual(input1[1], result[1]);
+			//Assert::AreEqual(input1[2], result[2]);
+
+			////test with invalid input
+			//double input2[3] = { 3,-4,5 };
+
+		}
+	};
+}
---
+++ TriangleUnitTests/TriangleUnitTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TriangleUnitTests
+{
+	TEST_CLASS(TriangleUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
